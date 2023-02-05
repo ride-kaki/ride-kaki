c5dbf0b17372024a67408cd52a9fc4cd948a91b6
soonann <sudo@soonann.dev>
2023-02-06 01:17:46 +0800

    feat: added polylines and pins
    - added polyline library
    - added smu and jurong latlngs in locations.dart

--- lib/utils/locations.dart
+++ lib/utils/locations.dart
@@ -1,6 +1,9 @@
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+const LatLng latLngJurongGateway = LatLng(1.3327, 103.74355);
+const LatLng latLngSMU = LatLng(1.2963, 103.8502);
+
 const CameraPosition locationGooglePlex = CameraPosition(
-  target: LatLng(1.2963, 103.8502),
+  target: latLngSMU,
   zoom: 14.0,
 );
