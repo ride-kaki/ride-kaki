24d64d32a1699ad26a2e76066ce570c8762489bb
soonann <sudo@soonann.dev>
2023-02-08 18:53:25 +0800

    feat: modified style of page to align with figma design

--- lib/screens/home/search_button.dart
+++ lib/screens/home/search_button.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 class SearchButton extends StatelessWidget {
   final Function() onTap;
   final String locationText;
-  final IconData iconData;
+
+  static const textBoxBorderColor = Color(0xFFE8E8E8);
+  static const textBoxBackgroundColor = Color(0xFFF6F6F6);
+  static const textBoxTextColor = Color(0xFFBDBDBD);
+  static const double borderRadius = 100.0;
 
   const SearchButton({
     super.key,
     required this.onTap,
     required this.locationText,
-    required this.iconData,
   });
 
   @override
@@ -18,30 +21,30 @@ class SearchButton extends StatelessWidget {
       cursorColor: Colors.grey,
       decoration: InputDecoration(
         hintText: locationText,
-        prefixIcon: Icon(
-          iconData,
-          color: Colors.grey[800],
-        ),
-        fillColor: Colors.white,
+        /*prefixIcon: Icon(*/
+        /*iconData,*/
+        /*color: Colors.grey[800],*/
+        /*),*/
+        fillColor: textBoxBackgroundColor,
         enabledBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: textBoxTextColor),
+          borderRadius: BorderRadius.circular(borderRadius),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: textBoxTextColor),
+          borderRadius: BorderRadius.circular(borderRadius),
         ),
         border: OutlineInputBorder(
-          borderSide: const BorderSide(color: Colors.transparent),
-          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: textBoxTextColor),
+          borderRadius: BorderRadius.circular(borderRadius),
         ),
         filled: true,
-        hintStyle: TextStyle(
-          color: Colors.grey[800],
+        hintStyle: const TextStyle(
+          color: textBoxTextColor,
         ),
         contentPadding: const EdgeInsets.symmetric(
-          horizontal: 30,
-          vertical: 7,
+          horizontal: 20,
+          vertical: 20,
         ),
       ),
       readOnly: true,
